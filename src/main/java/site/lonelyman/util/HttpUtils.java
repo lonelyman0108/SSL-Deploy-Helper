fff1fd085af397ee40fd90af5eedb50f91da5a2a
LM <me@lonelyman.site>
2022-06-16 17:28:34 +0800

    update:清理无用方法
    (其实想测试下workflow

--- src/main/java/site/lonelyman/util/HttpUtils.java
+++ src/main/java/site/lonelyman/util/HttpUtils.java
@@ -5,8 +5,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import java.util.Map;
-
 /**
  * <p>
  * Http工具类
@@ -30,27 +28,6 @@ public class HttpUtils {
             return client.newCall(request).execute();
         } catch (Exception e) {
             log.error("doGet error", e);
-            return null;
-        }
-    }
-
-    public  static Response doPost(String url, String authorization, String body, Map<String, String> params) {
-        try {
-            StringBuilder sb = new StringBuilder();
-//            sb.append(url).append("?");
-            if (params != null) {
-                for (Map.Entry<String, String> entry : params.entrySet()) {
-                    sb.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
-                }
-            }
-            client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url(url)
-                    .post(okhttp3.RequestBody.create(sb.toString(),null))
-                    .build();
-            return client.newCall(request).execute();
-        } catch (Exception e) {
-            log.error("doPost error", e);
             return null;
         }
     }
